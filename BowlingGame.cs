4b2ed43cb09ece4ae373ae51ae4c8a3fa38e7ce7
achollet <Alexis@mbp-de-alexis.home>
2017-07-06 23:54:26 +0200

    Splitting Class in separate files

---
+++ BowlingGame.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace TDDKata
+{
+    public class BowlingGame
+    {
+        protected int _score;
+
+        public Frame[] Frames { get; set; }
+
+        public int Score()
+        {
+            for (int f = 0; f < Frames.Length; f++)
+            {
+                _score += Frames[f].FrameScore();
+
+                if (Frames[f].IsStrike)
+                    _score += Frames[f + 1].FrameScore();
+
+                if (Frames[f].IsSpare)
+                    _score += Frames[f + 1].Rolls[1];
+            }
+
+            return _score;
+        }
+    }
+}
---
+++ Frame.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace TDDKata
+{
+    public class Frame
+    {
+        public int[] Rolls { get; set; }
+
+        public bool IsStrike
+        {
+            get
+            {
+                if (Rolls.Length == 1)
+                    return true;
+
+                return false;
+            }
+        }
+
+        public bool IsSpare
+        {
+            get
+            {
+                if (Rolls.Length > 1 && Rolls.Sum() == 10)
+                    return true;
+
+                return false;
+            }
+
+        }
+
+        public int FrameScore()
+        {
+            return Rolls.Sum();
+        }
+
+
+    }
+}
